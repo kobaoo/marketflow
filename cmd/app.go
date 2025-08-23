b49f93b1a3ddf627588496e64f46c527808d6f02
kobaoo <koblandikz@gmail.com>
2025-08-23 20:49:24 +0500

    Ready Tcp Client: inversion dependency

--- cmd/app.go
+++ cmd/app.go
@@ -5,17 +5,21 @@ import (
 	"fmt"
 	"log/slog"
 	"marketflow/internal/adapters/cache"
+	"marketflow/internal/adapters/exchange"
 	"marketflow/internal/adapters/postgres"
 	"marketflow/internal/adapters/web"
 	"marketflow/internal/config"
+	"marketflow/internal/domain"
 	"marketflow/internal/infra"
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
+	"time"
 )
 
 func RunApp() {
-	_, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer stop()
 
 	infra.SetUpLogger()
@@ -23,6 +27,18 @@ func RunApp() {
 	config, err := config.ReadConfig() // Read config
 	if err != nil {
 		slog.Error("Config Error", "error", err)
+	}
+	priceCh := make(chan *domain.PriceTick, 100)
+	defer close(priceCh)
+
+	var streams []domain.ExchangeStream
+	for _, exchangeCfg := range config.Exchanges {
+		stream := exchange.NewTCPHandler(
+			exchangeCfg,
+			priceCh,
+			5*time.Second,
+		)
+		streams = append(streams, stream)
 	}
 
 	rdb, err := cache.NewRedisClient(&config) // Connect to Redis
@@ -35,12 +51,66 @@ func RunApp() {
 	repository := postgres.NewRepository(db)
 	fmt.Println(repository, rdb)
 
-	// exchange.RunTCPClients(&config, rdb, false)
+	var wg sync.WaitGroup
+	for _, stream := range streams {
+		wg.Add(1)
+		go func(s domain.ExchangeStream) {
+			defer wg.Done()
+			slog.Info("Starting exchange stream", "exchange", s.GetExchangeName())
+			s.Start(ctx)
+		}(stream)
+	}
 
-	
+	// TODO: Processor to accept and aggregate data coming from priceCH channel
 
-	err = web.StartServer(&config)
-	if err != nil {
-		slog.Error("Error starting server", "error", err)
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		processPriceTicks(ctx, priceCh)
+	}()
+
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		slog.Info("Starting web server", "port", config.Port)
+		if err := web.StartServer(&config); err != nil {
+			slog.Error("Web server error", "error", err)
+			stop()
+		}
+	}()
+
+	<-ctx.Done()
+	slog.Info("Shutting down application...")
+
+	for _, stream := range streams {
+		stream.Stop()
+	}
+
+	wg.Wait()
+	slog.Info("Application stopped gracefully")
+}
+
+// it's just for checking tcpClient
+func processPriceTicks(
+	ctx context.Context, 
+	priceCh <-chan *domain.PriceTick,
+) {
+	for {
+		select {
+		case <-ctx.Done():
+			slog.Info("Price tick processor stopped")
+			return
+		case tick, ok := <-priceCh:
+			if !ok {
+				return
+			}
+			
+			slog.Debug("Price tick processed", 
+				"exchange", tick.Exchange, 
+				"symbol", tick.Symbol,
+				"price", tick.Price,
+				"stack of channel", len(priceCh))
+		}
 	}
 }
