3a9b1b44f2a2161209367ab6c03f319ecf644515
kobaoo <koblandikz@gmail.com>
2025-08-25 16:16:06 +0500

    config: from config.json

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,7 +2,6 @@ package config
 
 import (
 	"encoding/json"
-	"fmt"
 	"os"
 )
 
@@ -28,7 +27,6 @@ type RedisConfig struct {
 type PostgresConfig struct {
 	Dsn string `json:"dsn"`
 }
-
 func loadConfig(path string) (Config, error) {
 	var config Config
 	data, err := os.ReadFile(path)
@@ -45,55 +43,5 @@ func ReadConfig() (Config, error) {
 	if err != nil {
 		return config, err
 	}
-
-	// Override with environment variables for Docker
-	applyEnvOverrides(&config)
 	return config, nil
-}
-
-func applyEnvOverrides(config *Config) {
-	// Override Redis configuration
-	if redisHost := os.Getenv("REDIS_HOST"); redisHost != "" {
-		redisPort := os.Getenv("REDIS_PORT")
-		if redisPort == "" {
-			redisPort = "6379"
-		}
-		config.Redis.Addr = fmt.Sprintf("%s:%s", redisHost, redisPort)
-	}
-
-	// Override PostgreSQL configuration
-	if dbHost := os.Getenv("DB_HOST"); dbHost != "" {
-		dbUser := os.Getenv("DB_USER")
-		dbPass := os.Getenv("DB_PASS")
-		if dbUser == "" {
-			dbUser = "postgres"
-		}
-		if dbPass == "" {
-			dbPass = "secret"
-		}
-		config.Postgres.Dsn = fmt.Sprintf("postgres://%s:%s@%s:5432/mydb?sslmode=disable", dbUser, dbPass, dbHost)
-	}
-
-	// Override exchange configurations
-	if ex1Host := os.Getenv("EXCHANGE1_HOST"); ex1Host != "" {
-		for i := range config.Exchanges {
-			if config.Exchanges[i].Name == "ex1" {
-				config.Exchanges[i].Addr = fmt.Sprintf("%s:40101", ex1Host)
-			}
-		}
-	}
-	if ex2Host := os.Getenv("EXCHANGE2_HOST"); ex2Host != "" {
-		for i := range config.Exchanges {
-			if config.Exchanges[i].Name == "ex2" {
-				config.Exchanges[i].Addr = fmt.Sprintf("%s:40102", ex2Host)
-			}
-		}
-	}
-	if ex3Host := os.Getenv("EXCHANGE3_HOST"); ex3Host != "" {
-		for i := range config.Exchanges {
-			if config.Exchanges[i].Name == "ex3" {
-				config.Exchanges[i].Addr = fmt.Sprintf("%s:40103", ex3Host)
-			}
-		}
-	}
 }
